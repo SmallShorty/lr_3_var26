e4b1ff76a044e0e5987ca00ae96027d069d5406c
SmallShorty <46626284+SmallShorty@users.noreply.github.com>
2023-11-02 15:08:37 +0300

    чтение с файла

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ def is_valid_snils(num):
     pattern = r'^\d{3}-\d{3}-\d{3}\s\d{2}$'
     return bool(re.match(pattern, num))
 
-snils = ''
-
-while snils != '0':
-    snils = input('Введите номер СНИЛС: ')
-    if is_valid_snils(snils):
-        print('Номер СНИЛС корректен')
-    else:
-        print('Номер СНИЛС некорректен')
+with open('snils.txt', 'r') as file:
+    for line in file:
+        snils = line.strip()
+        if is_valid_snils(snils):
+            print(snils, 'Номер СНИЛС корректен')
+        else:
+            print(snils, 'Номер СНИЛС некорректен')
