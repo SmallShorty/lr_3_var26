36db10231c8cb3d34af2429f593de3934ea9421e
SmallShorty <46626284+SmallShorty@users.noreply.github.com>
2023-11-14 15:25:49 +0300

    add function control summ

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import re
 
+def control_sum(num):
+    numbers = [int(d) for d in num if d.isdigit()][:9]
+    weights = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+    control_sum = sum([a * b for a, b in zip(numbers[:9], weights)])
+    if control_sum < 100:
+        return control_sum == numbers[9] * 10 + numbers[10]
+    elif control_sum == 100 or control_sum == 101:
+        return numbers[9] == 0 and numbers[10] == 0
+    else:
+        return False
+
 def is_valid_snils(num):
     pattern = r'^\d{3}-\d{3}-\d{3}\s\d{2}$'
-    return bool(re.match(pattern, num))
+    return bool(re.match(pattern, num))+bool(control_sum(num))
 
-with open('snils.txt', 'r') as file:
+with open('data.txt', 'r') as file:
     for line in file:
         snils = line.strip()
         if is_valid_snils(snils):
